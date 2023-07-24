2dae8a25063ceebe63fd613aae8968d2ca6d4a5f
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-07-24 07:37:22 -0300

    feat(api): create APi component and api utils

--- src/components/App.js
+++ src/components/App.js
@@ -103,27 +103,6 @@ function App() {
         </li>
       </template>
 
-      {/* MODAL ZOOM PHOTOS */}
-
-      <section id="modal-photo" className="popup">
-        <div className="popup__photo-container">
-          <button
-            className="button button_close button_close_photo"
-            type="button"
-          >
-            <img
-              className="img img_button_close"
-              src={closeIcon}
-              alt="Ícone em forma de um X para fechar a janela de edição de perfil"
-            />
-          </button>
-          <figure className="popup__fig-container">
-            <img className="popup__img" />
-            <figcaption className="popup__figcaption"></figcaption>
-          </figure>
-        </div>
-      </section>
-
       {/* TEMPLATE LOADING CARDS */}
 
       <template id="cards-loading">
